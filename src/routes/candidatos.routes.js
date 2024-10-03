a4199ab80ae2bb1a93fc61b61dcd4af7ae045c68
vitor sampaio <vitorsampaio.tds2.senai@gmail.com>
2024-10-03 11:22:04 -0300

    feat: create post method

--- src/routes/candidatos.routes.js
+++ src/routes/candidatos.routes.js
@@ -3,41 +3,17 @@ import { Router } from "express"
 
 const candidatosRoutes = Router()
 
-const candidatos = [
+let candidatos = [
     {
-        id: Math.random() * 1000,
-        nome: "Jair Bolsonaro",
-        partido: "PSL",
-        idade: 67,
+        id: Math.floor(Math.random() * 1000),
+        nome: "Capitã Lucimara",
+        partido: "PSD",
+        idade: 39,
         segundo: true, // Concorrente ao segundo mandato
         propostas:[
             "Aumento da educação",
             "Aumento do saldo de contribuição",
-            "Aumento da conciliação"
-        ]
-    },
-    {
-        id: 2,
-        nome: "Fernando Haddad",
-        partido: "PT",
-        idade: 59,
-        segundo: true, // Concorrente ao segundo mandato
-        propostas: [
-            "Aumento da cooperação",
-            "apoio a educação",
-            "Aumento da estudo"
-        ]
-    },
-    {
-        id: 3,
-        nome: "Geraldo Alckmin",
-        partido: "PSDB",
-        idade: 68,
-        segundo: false, // Concorrente ao segundo mandato
-        propostas: [
-            "Aumento da educação",
-            "Aumento do saldo de contribuição",
-            "Aumento da conciliação"
+            "Aumento do salário"
         ]
     }
 ]
@@ -45,5 +21,51 @@ candidatosRoutes.get("/", (req, res) => {
     return res.status(200).send(candidatos)
 });
 
+candidatosRoutes.post("/", (req, res) => {
+    const { 
+        nome, 
+        partido,
+         idade,
+          segundo, 
+          propostas 
+} = req.body;
 
-export default candidatosRoutes;
+//validação dos campos nome
+
+if(!nome){
+    return res.status(400).send({message:"Candidato não tem nome pode jogar no lixo"})
+}
+
+//validaçao do partido
+if(!partido){
+    return res.status(400).send({message:"Candidato não tem partido pode jogar no lixo"})
+}
+
+// validação de idade
+if(idade < 18){
+    return res.status(400).send({message: nome + " Não pode participar dos debates pois ainda é menor de 18 anos e tem " + idade + " anos"})
+}
+
+// validação de todas as respostas
+if(!nome||!partido||!idade||!segundo||!propostas){
+    return res.status(400).send({message:"você é burro preencha todos os campos"})
+}
+// validação de propostas
+if(propostas == ""){
+    return res.status(400).send({message:"Candidato não tem nenhuma proposta não serve de nada pode jogar no lixo"})
+}
+const novoCandidato = {
+    id: Math.floor(Math.random() * 1000),
+    nome,
+    partido,
+    idade,
+    segundo,
+    propostas
+}
+
+candidatos.push(novoCandidato)
+
+return res.status(201).send("Candidato criado com sucesso")
+
+});
+export default candidatosRoutes
